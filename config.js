1e3ae110bc55075b62d330003d1deaf10fd0125a
rowill579 <165609461+rowill579@users.noreply.github.com>
2024-03-31 16:51:17 -0700

    ff
    ff

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'token': 'MTIyMzY3MDQ5MTk0NTEwNzYzNg.G4fdo_.DCNAj5omneN2tQWhcsJoxRKIUVyqxKd21JNO7o', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '1223670491945107636', // Your discord bot id.
+    'guildid': '1223671532535156767', // Id of a private server to load the bot's slash commands.
+    'ownerid': ['1204626364972670977'], // Add discord ids to the array to access the commands.
+    'webhook': 'https://discord.com/api/webhooks/1224122046640947280/dzNtaWjPxm_fowpcYvipaqEt9Mtnud5qG2nTxxyL2Ion86H3y6Hm3h790BFGii2KeXwi' // Your discord webhook url.
+}
